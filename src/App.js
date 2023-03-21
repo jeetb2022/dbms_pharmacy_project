5653205c2481cfd9b709ba555b9e832180ca59a0
Het Prajapati <het.p4@ahduni.edu.in>
2023-03-21 14:22:58 +0530

    Learning git

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 const  App = ()=> {
   return (
     <div className="App">
-     <h1>DBMS Project</h1>
+     <h1>DBMS database</h1>
      <h3>By Jeet Bhadaniya, Het Prajapati, Kushal Patel, Dhanashri Wala</h3>
     </div>
   );
