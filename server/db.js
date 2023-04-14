48f9aadc14fea6533f2e7a5acd2f5c3290bb9ca4
Jeet <jeetbhadaniya1228@gmail.com>
2023-04-14 13:47:37 +0530

    Feat : .env added; Single column data is getting added;

--- server/db.js
+++ server/db.js
@@ -1,8 +1,10 @@
 const Pool = require('pg').Pool;
+require('dotenv').config()
 
+// const pass = toString(process.env.pass);
 const pool = new Pool({
     user : "postgres",
-    password : "1234",
+    password : process.env.pass,
     host:"localhost",
     port : 5432,
     database : "DBByJeet"
--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express")
 const cors = require("cors");
+require('dotenv').config()
+const pool  = require('./db');
 const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -10,7 +12,12 @@ app.use(jsonParser);
 app.post('/add',(req,res)=>{
     res.json(req.body);
     console.log(req.body);
+    const data = pool.query(
+        "INSERT INTO person (id) VALUES($1)",
+        [req.body.intern_id]
+    );
 })
+
 
 
 app.listen((5000),()=>{
