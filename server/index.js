2d2c2ca543afbb0c7096767696bf9025c4718b7e
Jeet <jeetbhadaniya1228@gmail.com>
2023-04-19 18:13:58 +0530

    Feat : retail info to backend sent and updated in local  database

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.post('/add',async(req,res)=>{
     const query = await client.query(
         "INSERT INTO person (id,name) VALUES($1,$2) RETURNING *",
         [req.body.intern_id,req.body.intern_name]
-        );
+        );  
         res.json(query.rows[0]);
 })
 app.get('/add',async(req,res)=>{
@@ -30,9 +30,16 @@ app.post('/addMeds',async(req,res)=>{
         
 })
 app.post('/retailInfo',async(req,res)=>{
-        res.json(req.body);
         console.log(req.body);
+        let dataArray =[];
+        
+        const query = await client.query(
+            "INSERT INTO retailer_details (ret_fname, ret_lname,ret_shopname,ret_shop_address, ret_phone_number,ret_email,ret_password) VALUES($1,$2,$3,$4,$5,$6,$7) RETURNING *",
+            [...req.body,'jeet@gmail.com','jeetjeet']
+            );
+            // fname, lname,shopname,shopaddress, phone
 
+            res.json(req.body);
 })
 
 app.get('/add/:id',async(req,res)=>{
